d32520feaf8c47d1dd675eac514135723c2133d8
Monos <monmadnesss@gmail.com>
2025-01-04 11:25:18 +0700

    Refactored lab.py and lec.py into modular classes

--- lab.py
+++ lab.py
@@ -2,57 +2,65 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException, NoSuchElementException, ElementNotInteractableException
 
-NIM = "YOUR_NIM"
-PASSWORD = "YOUR_PASSWORD"
+class LabQuestionnaire:
 
-URL = "https://bluejack.binus.ac.id/prk/auth/login"
-driver = None
+    URL = "https://bluejack.binus.ac.id/prk/auth/login"
 
-def main():
-    driver.get(URL)
+    def __init__(self, driver: webdriver, credentials: dict[str, str], timeout: float=10.0):
+        self.nim = credentials["NIM"]
+        self.password = credentials["PASSWORD"]
+        self.driver = driver
+        self.wait = WebDriverWait(self.driver, timeout)
+    
+    def _load_page(self) -> None:
+        self.driver.get(self.URL)
+    
+    def _login(self) -> None:
+        self.wait.until(EC.title_contains('Login'))
 
-    WebDriverWait(driver, 10).until(EC.title_contains('Login'))
+        self.wait.until(
+            EC.presence_of_element_located((By.NAME, 'username'))
+        ).send_keys(self.nim)
 
-    WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located((By.NAME, 'username'))
-    ).send_keys(NIM)
+        self.wait.until(
+            EC.presence_of_element_located((By.NAME, 'password'))
+        ).send_keys(self.password)
 
-    WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located((By.NAME, 'password'))
-    ).send_keys(PASSWORD)
+        self.wait.until(
+            EC.element_to_be_clickable((By.XPATH, "//button[contains(text(), 'Login')]"))
+        ).click()
 
-    WebDriverWait(driver, 10).until(
-        EC.element_to_be_clickable((By.XPATH, "//button[contains(text(), 'Login')]"))
-    ).click()
+    def _fill_questionnaire(self) -> None:
+        scores = self.wait.until(EC.presence_of_all_elements_located((By.XPATH, "//input[@value='4']")))
+        for score in scores:
+            score.click()
 
-    try:
-        WebDriverWait(driver, 10).until(EC.title_contains('Questionnaire'))
-    except:
-        raise Exception("No questionnaire available!")
+        self.wait.until(
+            EC.presence_of_element_located((By.XPATH, "//button[contains(text(), 'Save')]"))
+        ).click()
+        self.wait.until(EC.title_contains('Praktikum'))
 
-    score_ins = WebDriverWait(driver, 10).until(
-        EC.presence_of_all_elements_located((By.XPATH, "//input[@value='4']"))
-    )
-
-    for score_in in score_ins:
-        score_in.click()
-
-    WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located((By.XPATH, "//button[contains(text(), 'Save')]"))
-    ).click()
-
-    WebDriverWait(driver, 10).until(
-        EC.title_contains('Praktikum')
-    )
-
-    print("Finished.")
+    def start(self) -> bool:
+        try:
+            self._load_page()
+            print("Logging in to your account.")
+            self._login()
+            print("Successfully logged in, taking questionnaire...")
+            self._fill_questionnaire()
+            print("Finished taking all questionnaire.")
+            return True
+        except Exception as e:
+            # TODO: Better error handling
+            print("An error occured: ", e)
+            return False
+        finally:
+            self.driver.close()
 
 if __name__ == '__main__':
-    try:
-        driver = webdriver.Chrome()
-        main()
-    except Exception as e:
-        print(e)
-    finally:
-        driver.quit()
+    import json
+    with open('config.json') as config_file:
+        credentials = json.load(config_file)
+    lab = LabQuestionnaire(webdriver.Chrome(), credentials)
+    lab.start()
--- lec.py
+++ lec.py
@@ -4,112 +4,100 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from time import sleep
 
-EMAIL = "YOUR_EMAIL"
-PASSWORD = "YOUR_PASSWORD"
+class LecQuestionnaire:
 
-URL = "https://qmc.apps.binus.ac.id/survey_list?isMandatory=1"
-driver = webdriver.Chrome()
+    URL = "https://qmc.apps.binus.ac.id/survey_list?isMandatory=1"
 
-def signin() -> bool:
-    try:
-        # TODO: Check if already signed in (EdgeDriver)
-        pass
-
-    except:
-        return True
+    def __init__(self, driver: webdriver, credentials: dict[str, str], timeout: float=15.0):
+        self.driver = driver
+        self.email = credentials["EMAIL"]
+        self.password = credentials["PASSWORD"]
+        self.wait = WebDriverWait(self.driver, timeout)
     
-    else:
+    def _load_page(self) -> None:
+        self.driver.get(self.URL)
+
+    def _signin(self) -> None:
+        # EMAIL
+        self.wait.until(
+            EC.presence_of_element_located((By.XPATH, "//input[@type='email']"))
+        ).send_keys(self.email)
+
+        self.wait.until(
+            EC.element_to_be_clickable((By.ID, 'idSIButton9'))
+        ).click()
+
+        sleep(1)
+
+        # PASSWORD
+        self.wait.until(
+            EC.presence_of_element_located((By.XPATH, "//input[@type='password']"))
+        ).send_keys(self.password)
+
+        self.wait.until(
+            EC.element_to_be_clickable((By.ID, 'idSIButton9'))
+        ).click()
+
+        sleep(1)
+
+        # SUBMIT
+        self.wait.until(
+            EC.element_to_be_clickable((By.ID, 'idSIButton9'))
+        ).click()
+
+    def _take_survey(self) -> bool:
+        hasSurvey = False
+
+        while True:
+            try:
+                # " Take Survey " btn
+                self.wait.until(
+                    EC.element_to_be_clickable((By.CSS_SELECTOR, 'button.btn.btn-primary.btn-sm.w-100'))
+                ).click()
+
+                # " Start " btn
+                self.wait.until(
+                    EC.element_to_be_clickable((By.CSS_SELECTOR, 'a.btn.btn-primary.btn-cons.btn-start'))
+                ).click()
+
+                scores = self.wait.until(
+                    EC.presence_of_all_elements_located((By.CSS_SELECTOR, "input[type='radio'][id$='6']"))
+                )
+
+                for score in scores: # hidden element
+                    self.driver.execute_script('arguments[0].click();', score)
+
+                # " Done " btn
+                self.wait.until(
+                    EC.element_to_be_clickable((By.XPATH, "//div[@class='button-done']/button[contains(@class, 'btn-primary')]"))
+                ).click()
+
+                hasSurvey = True
+                # redirected back to URL
+            except:
+                return hasSurvey
+    
+    def start(self) ->  bool:
         try:
-            # EMAIL
-            WebDriverWait(driver, 10).until(
-                EC.presence_of_element_located((By.XPATH, "//input[@type='email']"))
-            ).send_keys(EMAIL)
-
-            WebDriverWait(driver, 10).until(
-                EC.element_to_be_clickable((By.ID, 'idSIButton9'))
-            ).click()
-
-            sleep(1)
-
-            # PASSWORD
-            WebDriverWait(driver, 10).until(
-                EC.presence_of_element_located((By.XPATH, "//input[@type='password']"))
-            ).send_keys(PASSWORD)
-
-            WebDriverWait(driver, 10).until(
-                EC.element_to_be_clickable((By.ID, 'idSIButton9'))
-            ).click()
-
-            sleep(1)
-
-            # SUBMIT
-            WebDriverWait(driver, 10).until(
-                EC.element_to_be_clickable((By.ID, 'idSIButton9'))
-            ).click()
-
-
-        except Exception as e:
-            print(e)
-            return False
-        
-        else:
+            self._load_page()
+            print("Signing in to your account.")
+            self._signin()
+            print("Successfully signed in, taking survey...")
+            if not self._take_survey():
+                print("Failed to load survey or none are available.")
+                return False
+            print("Finished taking all survey.")
             return True
+        except Exception as e:
+            # TODO: Better error handling
+            print("An error occured: ", e)
+            return False
+        finally:
+            self.driver.quit()
 
-def take_survey() -> bool:
-    hasSurvey = False;
-
-    while True:
-        try:
-            # " Take Survey " btn
-            WebDriverWait(driver, 10).until(
-                EC.element_to_be_clickable((By.CSS_SELECTOR, 'button.btn.btn-primary.btn-sm.w-100'))
-            ).click()
-
-            # " Start " btn
-            WebDriverWait(driver, 10).until(
-                EC.element_to_be_clickable((By.CSS_SELECTOR, 'a.btn.btn-primary.btn-cons.btn-start'))
-            ).click()
-
-            score_ins = WebDriverWait(driver, 10).until(
-                EC.presence_of_all_elements_located((By.CSS_SELECTOR, "input[type='radio'][id$='6']"))
-            )
-
-            for score_in in score_ins: # hidden element
-                driver.execute_script('arguments[0].click();', score_in)
-
-            # " Done " btn
-            WebDriverWait(driver, 10).until(
-                EC.element_to_be_clickable((By.XPATH, "//div[@class='button-done']/button[contains(@class, 'btn-primary')]"))
-            ).click()
-            
-            hasSurvey = True
-            
-            # redirected back to URL
-
-        except:
-            return hasSurvey
-
-def main():
-    try:
-        driver.get(URL)
-
-        if (signin()):
-            print("Successfully signed in.")
-        else:
-            print("Failed to sign in.")
-            return
-        
-        if (take_survey()):
-            print("Finished taking survey.")
-        else: 
-            print("Failed to load survey or none are available.")
-            return
-
-    except Exception as e:
-        print(e)
-
-    finally:
-        driver.close()
-
-if __name__ == "__main__":
-    main()
+if __name__ == '__main__':
+    import json
+    with open('config.json') as config_file:
+        credentials = json.load(config_file)
+    lec = LecQuestionnaire(webdriver.Chrome(), credentials)
+    lec.start()
